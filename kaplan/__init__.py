a0dee3b065d9b1a01294518433389ba27f09b1c8
Jen <jgarner.py@gmail.com>
2018-11-27 13:13:44 -0500

    update init for kaplan

--- kaplan/__init__.py
+++ kaplan/__init__.py
@@ -1,2 +1,14 @@
-from kaplan.mutations import mutate
+# here is a list of all of the functions and objects in kaplan
+# this list is imported when the user writes "from kaplan import *"
+from kaplan.energy import run_energy_calc, prep_psi4_geom
+from kaplan.fitg import get_fitness, sum_energies, sum_rmsds, all_pairs_gen, calc_fitness
+from kaplan.gac import run_kaplan
 from kaplan.ga_input import read_ga_input, verify_ga_input
+from kaplan.geometry import generate_parser, zmatrix_to_xyz, generate_zmatrix
+from kaplan.mol_input import read_mol_input, verify_mol_input
+from kaplan.mutations import generate_children, mutate, swap
+from kaplan.output import run_output
+from kaplan.pmem import Pmem
+from kaplan.ring import RingEmptyError, RingOverflowError, Ring
+from kaplan.rmsd import calc_rmsd
+from kaplan.tournament import run_tournament, select_pmems, select_parents
