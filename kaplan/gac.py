d40722ee19446b31ff9480e90d366312e62ecb8c
Jen Garner <jgarner.py@gmail.com>
2019-05-15 13:04:14 -0400

    add feature where user can add previous ring as input

--- kaplan/gac.py
+++ kaplan/gac.py
@@ -5,7 +5,8 @@ number of mating events.
 
 """
 
-import sys
+import os
+import pickle
 
 import numpy as np
 
@@ -15,13 +16,15 @@ from kaplan.tournament import run_tournament
 from kaplan.output import run_output
 from kaplan.energy import run_energy_calc
 
-def run_kaplan(input_dict, save=True, output_loc="pwd"):
+def run_kaplan(input_dict, ring=None, save=True, output_loc="pwd"):
     """Run the Kaplan programme.
 
     Parameters
     ----------
     input_dict : dict
         Contains inputs required to run Kaplan.
+    ring : str
+        File name for the pickled ring object.
     save : bool
         Defaults to True, which causes the program
         to write a pickle binary file to the output
@@ -33,6 +36,14 @@ def run_kaplan(input_dict, save=True, output_loc="pwd"):
         followed by a job directory. If home is specified,
         the output for the first job is written to:
         /home/username/kaplan_output/job_0).
+    
+    Raises
+    ------
+    AssertionError
+        The pickle file name for the ring structure
+        does not exist.
+        The number of slots is different than the
+        original number of slots for the ring.
 
     """
     # read in and verify inputs
@@ -42,15 +53,29 @@ def run_kaplan(input_dict, save=True, output_loc="pwd"):
     # check that initial geometry converges
     run_energy_calc(inputs.coords)
 
-    # make a ring; constructor fills
-    # ring with an initial population
-    ring = Ring(inputs.num_slots, inputs.init_popsize)
+    # biggest_bday is initial mating event number
+    # it is set to the age of the oldest exsiting pmem
+    biggest_bday = 0
+    if ring:
+        assert os.path.isfile(ring)
+        assert ring.num_slots == inputs.num_slots
+        for pmem in ring:
+            if pmem.birthday > biggest_bday:
+                biggest_bday = pmem.birthday
+    else:
+        # make a ring; constructor fills
+        # ring with an initial population
+        ring = Ring(inputs.num_slots, inputs.init_popsize)
 
     # run the mevs
-    for mev in range(inputs.num_mevs):
+    for mev in range(biggest_bday, biggest_bday+inputs.num_mevs):
         try:
             print(f"Mating event: {mev}")
             run_tournament(ring, mev)
+            # write a temporary file every 10 mating events
+            if mev%10 == 0:
+                with open(f"temp_{inputs.struct_input}.pickle", "wb") as f:
+                    pickle.dump(ring, f)
         except RingEmptyError:
             ring.fill(inputs.init_popsize, mev)
 
